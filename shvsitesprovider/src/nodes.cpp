255b6c88e2502b6f142f8382c7d3753886611565
Václav Kubernát <vkubernat@elektroline.cz>
2024-12-11 15:56:14 +0100

    Revert "Update libshv"
    This reverts commit fa357c1e3f871b3f9ba2d1c502cc66960baaf0b3.

--- shvsitesprovider/src/nodes.cpp
+++ shvsitesprovider/src/nodes.cpp
@@ -200,10 +200,10 @@ cp::RpcValue AppRootNode::callMethodRq(const cp::RpcRequest &rq)
 		return readFileCompressed(rq);
 	}
 	else if (method == METH_FILE_SIZE) {
-		return readFile(rq.shvPath().to<QString>()).asBlob().size();
+		return readFile(rq.shvPath().to<QString>()).asData().second;
 	}
 	else if (method == METH_FILE_SIZE_COMPRESSED) {
-		return readFileCompressed(rq).asBlob().size();
+		return readFileCompressed(rq).asData().second;
 	}
 	else if (method == METH_FILE_HASH) {
 		string bytes = readFile(qshv_path).toString();
@@ -627,9 +627,9 @@ shv::chainpack::RpcValue AppRootNode::readFileCompressed(const shv::chainpack::R
 	}
 
 	cp::RpcValue result;
-	const auto& blob = readFile(request.shvPath().to<QString>()).asBlob();
+	const auto blob = readFile(request.shvPath().to<QString>()).asData();
 	if (compression_type == CompressionType::QCompress) {
-		const auto compressed_blob = qCompress(QByteArray::fromRawData(reinterpret_cast<const char*>(blob.data()), static_cast<int>(blob.size())));
+		const auto compressed_blob = qCompress(QByteArray::fromRawData(blob.first, static_cast<int>(blob.second)));
 		result = cp::RpcValue::Blob(compressed_blob.cbegin(), compressed_blob.cend());
 	}
 
