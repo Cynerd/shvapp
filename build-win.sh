faa42856c2b3dcc1da0882b522e9a06bf65a9925
Ondřej Brožek <obrozek@elektroline.cz>
2025-01-15 12:45:18 +0000

    Update build-win.sh

--- build-win.sh
+++ build-win.sh
@@ -28,5 +28,5 @@ pushd
 # TODO: Reenable these, if we're ever going to install this somewhere. Also port them to windeployqt!
 "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "-DVERSION=${BFSVIEW_VERSION}" clients/bfsview/bfsview.iss || exit /b 2
 "C:\Program Files (x86)\Inno Setup 5\iscc.exe" "-DVERSION=${JN50VIEW_VERSION}" clients/jn50view/jn50view.iss || exit /b 2
-JN50VIEW_VERSION=`grep APP_VERSION clients/jn50view/src/version.h | cut -d\" -f2`
-BFSVIEW_VERSION=`grep APP_VERSION clients/bfsview/src/version.h | cut -d\" -f2`
+# JN50VIEW_VERSION=`grep APP_VERSION clients/jn50view/src/version.h | cut -d\" -f2`
+# BFSVIEW_VERSION=`grep APP_VERSION clients/bfsview/src/version.h | cut -d\" -f2`
