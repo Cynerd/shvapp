3c56d1a6910350eabfd77c5cff08a7d1a4298385
Václav Kubernát <vkubernat@elektroline.cz>
2025-02-14 10:07:53 +0100

    history: Also log offending path instead of just the slave hp

--- historyprovider3/src/shvjournalnode.cpp
+++ historyprovider3/src/shvjournalnode.cpp
@@ -201,7 +201,7 @@ void ShvJournalNode::onRpcMessageReceived(const cp::RpcMessage &msg)
 			if (it != m_slaveHps.end()) {
 				{
 					if (path.at(it->shv_path.size()) != '/') {
-						journalWarning() << "Discarding notification with a top-level node path. Offending path was:" << it->shv_path;
+						journalWarning() << "Discarding notification with a top-level node path. Offending path was:" << path << "slave hp:" << it->shv_path;
 						return;
 					}
 
