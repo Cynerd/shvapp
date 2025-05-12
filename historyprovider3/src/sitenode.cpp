6e496a16143bddd34dbe0ecc175080d22823986e
Václav Kubernát <vkubernat@elektroline.cz>
2025-05-12 16:54:37 +0200

    history: Send signal only after setting online_status
    It doesn't matter in this case, but logically, this order is better.

--- historyprovider3/src/sitenode.cpp
+++ historyprovider3/src/sitenode.cpp
@@ -143,9 +143,9 @@ void SiteNode::setOnlineStatus(const OnlineStatus online_status)
 			alarm.stale = true;
 		}
 	}
+	m_onlineStatus = online_status;
 
 	HistoryApp::instance()->rpcConnection()->sendShvSignal(shvPath().asString(), M_ONLINE_STATUS_CHNG, shv::chainpack::RpcValue::Int(online_status));
-	m_onlineStatus = online_status;
 }
 
 SiteNode::SiteNode(const std::string& node_id, const std::string& journal_cache_dir, LogType log_type, ShvNode* parent)
